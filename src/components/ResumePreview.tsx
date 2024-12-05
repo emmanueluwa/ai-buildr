eaa3df46a6501bf26157d31a01a41d9c530281bc
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-12-05 03:07:36 +0000

    feat: adding zoom for text dependent on responsive page size

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,5 +1,7 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
+import { useRef } from "react";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -10,15 +12,29 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(containerRef);
+
   return (
     <div
       className={cn(
+        // 210/297 a4 sheet
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={containerRef}
     >
-      <div className="p-6 text-3xl font-bold">
-        text changes with size of container
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{
+          // 210 mm => 704 pixels
+          zoom: (1 / 794) * width,
+        }}
+      >
+        <h1 className="p-6 text-3xl font-bold">
+          text changes with size of container
+        </h1>
       </div>
     </div>
   );
