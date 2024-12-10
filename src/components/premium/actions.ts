a6499c5458f3dd2863c49f7e65681c6beb1ef9dd
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-12-10 20:24:42 +0000

    feat: adding stripe checkout

--- src/components/premium/actions.ts
+++ src/components/premium/actions.ts
@@ -1,11 +1,10 @@
-"user server";
+"use server";
 
 import stripe from "@/lib/stripe";
 import { currentUser } from "@clerk/nextjs/server";
 
 export async function createCheckoutSession(priceId: string) {
   const user = await currentUser();
-
   if (!user) {
     throw new Error("Unauthorized");
   }
