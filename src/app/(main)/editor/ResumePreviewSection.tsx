70fadc53ba26ea19b04647f8a21021564a2d13d3
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-12-31 21:46:10 +0000

    feat: updating colour picker position on ui

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -19,7 +19,7 @@ export default function ResumePreviewSection({
     <div
       className={cn("group relative hidden w-full md:flex md:w-1/2", className)}
     >
-      <div className="absolute right-1 top-1 flex flex-none flex-col gap-3 opacity-50 transition-opacity group-hover:opacity-100 lg:left-3 lg:top-3 xl:opacity-100">
+      <div className="absolute right-1 top-1 flex flex-none flex-col gap-3 opacity-50 transition-opacity group-hover:opacity-100 lg:right-3 lg:top-3 xl:opacity-100">
         <ColorPicker
           color={mealplanData.colorHex}
           onChange={(color) =>
